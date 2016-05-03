110a3acab46074dc20dd8923cdb6106a43b36f20
warddem <warddem@gmail.com>
2016-05-03 20:37:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -37,6 +37,35 @@ module.exports = new Script({
             function getSilent() {
                 return bot.getProp("silent");
             }
+            
+            /* getReply should allow for some variety in responses for received text messages that 
+do not have an entry in the scripts.json file. */
+            function getReply() {
+                var messages = [ "Sorry. I'm not configured with a response to your message. Text COMMANDS to see a few examples.",
+                                 "Hey, I didn't understand that either. I suggest sending HELP OUT",
+                                 "Text me ABOUT to learn about the JBot project.",
+                                 "You're not sending messages to an Artifical Intelligence program. Try MORE to see some other COMMANDS",
+                                 "The program responds to COMMANDS only. You have to send a command that I understand. :)",
+                                 "The JBot is not a human. It is just a series of files on a computer. Text ABOUT to learn more.",
+                                 "Seriously, you are wayyyyy smarter than JBot. It just knows simple COMMANDS",
+                                 "Yo. I do not know what you are talking about. Send me a HELLO",
+                                 "There is a ton of information in JBot. You have to use COMMANDS to find it.",
+                                 "That's interesting. Hhhmmm... I never thought of that. Maybe try HELP OUT",
+                                 "Can you say that again?",
+                                 "Yeah... that happens from time to time. Try COMMANDS or ABOUT.",
+                                 "That is a ton of words you just wrote there... I really don't know. Try MORE",
+                                 "Right now, punctuation throws me off. Send text without it. Try HELP OUT",
+                                 "Try sending a command without punctuation.",
+                                 "I'm not programmed to ignore punctuation. So if you're sending something other than letters... I don't understand it."
+                                ];
+
+                var arrayIndex = Math.floor( Math.random() * messages.length );
+
+
+                return messages[arrayIndex];
+                
+            }
+            
 
             function processMessage(isSilent) {
                 if (isSilent) {
